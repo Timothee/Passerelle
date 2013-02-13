d7ce43a6d38910f8b2cf6f7e28ca4b61e5ea7c1a
Timothée Boucher <timothee@gmail.com>
2013-02-12 23:55:03 -0800

    Add tests for proper git URLs (most of them at least)

--- passerelle_tests.py
+++ passerelle_tests.py
@@ -23,6 +23,25 @@ class PasserelleTestCase(unittest.TestCase):
 		assert response.status_code == 400
 		assert response.status == "400 Git 'from' URL Not Sent"
 	
+	def test_proper_git_urls(self):
+		correct_urls = ['ssh://user@host.com:8080/path/to/repo.git/',
+				'ssh://user@host.com:8080/~user/path/to/repo.git/',
+				'git://host.com:8080/path/to/repo.git',
+				'git://host.com:8080/~user/path/to/repo.git',
+				'https://host.com:8080/path/to/repo.git',
+				'ftp://host.com:8080/path/to/repo.git',
+				'rsync://host.com:8080/path/to/repo.git',
+				'git@host.com:username/repo.git',
+				'host.com/~/path/to/repo.git',
+				'/path/to/repo.git/',
+				'file:///path/to/repo.git']
+		wrong_urls = ['host.com/repo',
+				'rtmp://whatisthis.com/really.git']
+		for url in correct_urls:
+			print url, passerelle.check_git_url(url)
+			assert passerelle.check_git_url(url) is True
+		for url in wrong_urls:
+			assert passerelle.check_git_url(url) is False
 
 if __name__ == '__main__':
     unittest.main()
